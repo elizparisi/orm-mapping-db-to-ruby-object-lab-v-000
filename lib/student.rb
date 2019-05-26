162eb80605fa2069d83ad9402b43895490965735
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-05-26 01:54:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -117,5 +117,12 @@ class Student
     sql = <<-SQL 
       SELECT * 
       FROM students 
+      WHERE grade = ?;
+    SQL
+    
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
   
 end
